5316aa3075c00c4398e6df14d54a84a88fe58b95
Qetuose <lukasbaltrimas123@gmail.com>
2024-09-05 14:17:38 +0300

    FormSpliBill reset on selecting other person

--- src/App.js
+++ src/App.js
@@ -77,6 +77,7 @@ export default function App() {
         <FormSpliBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
